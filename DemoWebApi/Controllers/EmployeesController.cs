26e25681d3535d636d8698a76398d1bf64df1b10
Emrullah IŞIK <emrullah04@outlook.com>
2023-07-23 22:24:33 +0300

    add post method and update get method

--- DemoWebApi/Controllers/EmployeesController.cs
+++ DemoWebApi/Controllers/EmployeesController.cs
@@ -1,6 +1,7 @@
 ﻿using EmployeeDataAccess;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web.Http;
 
 namespace DemoWebApi.Controllers
@@ -15,12 +16,43 @@ namespace DemoWebApi.Controllers
             }
         }
 
-        public Employee Get(int id)
+        public HttpResponseMessage Get(int id)
         {
             using (EmployeeDBEntities entities = new EmployeeDBEntities())
             {
-                return entities.Employees.FirstOrDefault(e=>e.ID == id);
+                var entity = entities.Employees.FirstOrDefault(e=>e.ID == id);
+
+                if (entity != null)
+                {
+                    return Request.CreateResponse(System.Net.HttpStatusCode.OK, entity);
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(System.Net.HttpStatusCode.NotFound, "Employee with Id" +  id.ToString() + "not found");
+                }
             }
+        }
+
+        public HttpResponseMessage Post([FromBody] Employee employee)
+        {
+            try
+            {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    entities.Employees.Add(employee);
+                    entities.SaveChanges();
+
+                    var message = Request.CreateResponse(System.Net.HttpStatusCode.Created, employee);
+                    message.Headers.Location = new System.Uri(Request.RequestUri + employee.ID.ToString());
+                    return message;
+                }
+            }
+            catch (System.Exception ex)
+            {
+
+                return Request.CreateErrorResponse(System.Net.HttpStatusCode.BadRequest, ex);
+            }
+
         }
     }
 }
