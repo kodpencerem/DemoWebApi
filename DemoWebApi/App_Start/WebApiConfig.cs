e93e70db790243bdb3669e921e79a549aa7b546d
Emrullah IŞIK <emrullah04@outlook.com>
2023-07-23 21:41:35 +0300

    add CustomJsonFormatter

--- DemoWebApi/App_Start/WebApiConfig.cs
+++ DemoWebApi/App_Start/WebApiConfig.cs
@@ -1,9 +1,27 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Web.Http;
 
 namespace DemoWebApi
 {
+    public class CustomJsonFormatter : JsonMediaTypeFormatter
+    {
+        public CustomJsonFormatter()
+        {
+            this.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
+        }
+
+        public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+        {
+            base.SetDefaultContentHeaders(type, headers, mediaType);
+            headers.ContentType = new MediaTypeHeaderValue("application/json");
+        }
+    }
+
     public static class WebApiConfig
     {
+        
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
@@ -17,7 +35,9 @@ namespace DemoWebApi
                 defaults: new { id = RouteParameter.Optional }
             );
 
-            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
+            config.Formatters.Add(new CustomJsonFormatter());
+
+            //config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
 
             //config.Formatters.Remove(config.Formatters.XmlFormatter);
 
